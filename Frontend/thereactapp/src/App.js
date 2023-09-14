1996210c921ce3785619eec449bb07294f64e01d
OliviaBurke <85521140+OliviaBurke@users.noreply.github.com>
2023-09-14 12:56:46 +0000

    Selected background color for homepage

--- Frontend/thereactapp/src/App.js
+++ Frontend/thereactapp/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import Homepage from './Homepage';
+import setBodyColor from './setBodyColor'
 
 function App() {
+
   return (
-    <div className="App">
-      Hello World
-    </div>
+
+    Homepage()
   );
 }
 
