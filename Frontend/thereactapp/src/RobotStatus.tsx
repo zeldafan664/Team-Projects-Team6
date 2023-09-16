79970def9e3a1230d3c24f7d008510ce164e9977
OliviaBurke <85521140+OliviaBurke@users.noreply.github.com>
2023-09-16 15:46:15 +0000

    Added boxes to display robot statuses

--- Frontend/thereactapp/src/RobotStatus.tsx
+++ Frontend/thereactapp/src/RobotStatus.tsx
@@ -3,13 +3,109 @@ import Box from '@mui/material/Box';
 import Button from '@mui/material/Button'
 export default function RobotStatus() {
     return (
-        <div style={
-            {
-                border: '2px solid red'
-            }
-        }>
-            Robot
-        </div>
+        <div style={{ display: "flex", justifyContent: "space-evenly" }}>
+            <Box
+                style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    border: "1px solid gray",
+                    width: "8rem",
+                    height: "8rem",
+                    borderRadius: "0.5rem",
+                    boxShadow: "3px 3px 8px gray"
+                }}
+            >
+                <div
+                    style={{
+                        display: "flex",
+                        alignSelf: "center",
+                        fontSize: "22px"
+                    }}
+                > Power Status
+                </div>
+            </Box >
+            <Box
+                style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    border: "1px solid gray",
+                    width: "8rem",
+                    height: "8rem",
+                    borderRadius: "0.5rem",
+                    boxShadow: "3px 3px 8px gray"
+                }}
+            >
+                <div
+                    style={{
+                        display: "flex",
+                        alignSelf: "center",
+                        fontSize: "22px"
+                    }}
+                > Task Status
+                </div>
+            </Box >
+            <Box
+                style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    border: "1px solid gray",
+                    width: "8rem",
+                    height: "8rem",
+                    borderRadius: "0.5rem",
+                    boxShadow: "3px 3px 8px gray"
+                }}
+            >
+                <div
+                    style={{
+                        display: "flex",
+                        alignSelf: "center",
+                        fontSize: "22px"
+                    }}
+                > Com. Status
+                </div>
+            </Box >
+            <Box
+                style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    border: "1px solid gray",
+                    width: "8rem",
+                    height: "8rem",
+                    borderRadius: "0.5rem",
+                    boxShadow: "3px 3px 8px gray"
+                }}
+            >
+                <div
+                    style={{
+                        display: "flex",
+                        alignSelf: "center",
+                        fontSize: "22px"
+                    }}
+                > Battery %
+                </div>
+            </Box >
+            <Box
+                style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    border: "1px solid gray",
+                    width: "8rem",
+                    height: "8rem",
+                    borderRadius: "0.5rem",
+                    boxShadow: "3px 3px 8px gray"
+                }}
+            >
+                <div
+                    style={{
+                        display: "flex",
+                        alignSelf: "center",
+                        fontSize: "22px"
+                    }}
+                > Task %
+                </div>
+            </Box >
+
+        </div >
     )
 
 }
