2d839f0b40a79bcf63df8feead546545a5dd6a78
OliviaBurke <85521140+OliviaBurke@users.noreply.github.com>
2023-09-19 12:32:43 +0000

    Added table for data

--- Frontend/thereactapp/src/UsageData.tsx
+++ Frontend/thereactapp/src/UsageData.tsx
@@ -1,21 +1,117 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TablePagination from '@mui/material/TablePagination';
+import TableRow from '@mui/material/TableRow';
 
-export default function UsageData() {
+const columns = [
+    { id: 'name', label: 'Last Used', minWidth: 50 },
+    { id: 'code', label: 'Runtime', minWidth: 50 },
+    {
+        id: 'anotherdatavalue',
+        label: 'Another Data Value',
+        minWidth: 50,
+        align: 'right',
+        format: (value: any) => value.toLocaleString('en-US'),
+    },
+    // {
+    //     id: 'size',
+    //     label: 'Size\u00a0(km\u00b2)',
+    //     minWidth: 170,
+    //     align: 'right',
+    //     format: (value: any) => value.toLocaleString('en-US'),
+    // },
+    // {
+    //     id: 'density',
+    //     label: 'Density',
+    //     minWidth: 170,
+    //     align: 'right',
+    //     format: (value: any) => value.toFixed(2),
+    // },
+];
+
+function createData(name: any, code: any, anotherdatavalue: any) {
+
+    return { name, code, anotherdatavalue };
+}
+
+const rows = [
+    createData('10/30/2023', '5:00', "XXXXX"),
+    createData('10/31/2023', '6:32', "XXXXX"),
+    createData('11/1/2023', '3:44', "XXXXX"),
+    createData('11/2/2023', '10:57', "XXXXX"),
+
+];
+
+export default function StickyHeadTable() {
+    const [page, setPage] = React.useState(0);
+    const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
+    const handleChangePage = (newPage: any) => {
+        setPage(newPage);
+    };
+
+    const handleChangeRowsPerPage = (event: any) => {
+        setRowsPerPage(+event.target.value);
+        setPage(0);
+    };
 
     return (
-        <Box style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            border: "1px solid gray",
-            width: "30rem",
-            height: "40rem",
-            borderRadius: "0.5rem",
+        <Paper sx={{
+            width: '50%', overflow: 'hidden', borderRadius: "0.5rem",
             boxShadow: "3px 3px 8px gray",
             backgroundColor: "#EEEEEE"
         }}>
-            Usage Data
-        </Box>
-    )
+            <TableContainer sx={{ maxHeight: 440 }}>
+                <Table stickyHeader aria-label="sticky table">
+                    <TableHead>
+                        <TableRow>
+                            {columns.map((column) => (
+                                <TableCell
+                                    key={column.id}
+                                    align={"center"}
+                                    style={{ minWidth: column.minWidth }}
+                                >
+                                    {column.label}
+                                </TableCell>
+                            ))}
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {rows
+                            .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                            .map((row: any) => {
+                                return (
+                                    <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
+                                        {columns.map((column) => {
+                                            const value = row[column.id];
+                                            return (
+                                                <TableCell key={column.id} align={"center"}>
+                                                    {column.format && typeof value === 'number'
+                                                        ? column.format(value)
+                                                        : value}
+                                                </TableCell>
+                                            );
+                                        })}
+                                    </TableRow>
+                                );
+                            })}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+            <TablePagination
+                rowsPerPageOptions={[10, 25, 100]}
+                component="div"
+                count={rows.length}
+                rowsPerPage={rowsPerPage}
+                page={page}
+                onPageChange={handleChangePage}
+                onRowsPerPageChange={handleChangeRowsPerPage}
+            />
+        </Paper>
+    );
 }
