a638ad330400794c9f34b3183f0e2e07f27feae6
OliviaBurke <85521140+OliviaBurke@users.noreply.github.com>
2023-09-19 12:54:29 +0000

    Added tooltip for user guide

--- Frontend/thereactapp/src/Homepage.tsx
+++ Frontend/thereactapp/src/Homepage.tsx
@@ -5,12 +5,18 @@ import Box from '@mui/material/Box';
 import PieDisplay from './PieChart';
 import InfoDisplay from './InfoChart';
 import UsageData from './UsageData';
+import Tooltip from '@mui/material/Tooltip';
+import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
 
 function Homepage() {
     setBodyColor({ color: "#d9d9d9" })
 
     return (
         <>
+            <Tooltip title="User Guide">
+                <HelpOutlineIcon></HelpOutlineIcon>
+            </Tooltip>
+
             <RobotStatus />
             <Box sx={{ display: "flex", justifyContent: "space-evenly", alignItems: "center", marginTop: "5rem" }}>
                 <UsageData />
