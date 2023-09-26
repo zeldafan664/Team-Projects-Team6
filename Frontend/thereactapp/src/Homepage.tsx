4d4f378d8dadad34eb4587d5343a96fa9b69aeb0
LiamPC <liam@LiamPC.localdomain>
2023-09-26 08:14:14 -0400

    adding code for backend communication with frontend

--- Frontend/thereactapp/src/Homepage.tsx
+++ Frontend/thereactapp/src/Homepage.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { useEffect, useState } from 'react';
 import RobotStatus from "./RobotStatus"
 import setBodyColor from "./setBodyColor"
 import Box from '@mui/material/Box';
@@ -9,7 +9,25 @@ import Tooltip from '@mui/material/Tooltip';
 import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
 
 function Homepage() {
-    setBodyColor({ color: "#d9d9d9" })
+    const [message, setMessage] = useState("");
+
+    // Fetch the message from the backend when the component mounts
+    useEffect(() => {
+        fetch('/api/message')
+            .then((response) => response.text())
+            .then((data) => {
+                // Set the message state with the data from the backend
+                setMessage(data);
+            })
+            .catch((error) => {
+                console.error('Error fetching message:', error);
+            });
+    }, []);
+
+    // Set the body color
+    useEffect(() => {
+        setBodyColor({ color: "#d9d9d9" });
+    }, []);
 
     return (
         <>
@@ -17,16 +35,17 @@ function Homepage() {
                 <HelpOutlineIcon></HelpOutlineIcon>
             </Tooltip>
 
+            <h1>Message from Backend:</h1>
+            <p id="message">{message}</p>
+
             <RobotStatus />
             <Box sx={{ display: "flex", justifyContent: "space-evenly", alignItems: "center", marginTop: "5rem" }}>
                 <UsageData />
                 <PieDisplay />
                 <InfoDisplay />
-
             </Box>
         </>
-
     )
 }
 
-export default Homepage
+export default Homepage;
