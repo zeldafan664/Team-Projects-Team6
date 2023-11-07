422fa3c7a8377a62db0297396cf512e531722297
OliviaBurke <85521140+OliviaBurke@users.noreply.github.com>
2023-11-07 13:23:05 +0000

    Removed button functionality from robot statuses

--- Frontend/thereactapp/src/RobotStatus.tsx
+++ Frontend/thereactapp/src/RobotStatus.tsx
@@ -7,79 +7,13 @@ import StartStopButton from './StartStopButton';
 
 export default function RobotStatus() {
 
-    const [active1, setActive1] = useState(false);
-    const [active2, setActive2] = useState(false);
-    const [active3, setActive3] = useState(false);
-    const [active4, setActive4] = useState(false);
-    const [active5, setActive5] = useState(false);
-
-    const handleClick1 = () => {
-        setActive1(current => !current);
-    };
-    const handleClick2 = () => {
-        setActive2(current => !current);
-    };
-    const handleClick3 = () => {
-        setActive3(current => !current);
-    };
-    const handleClick4 = () => {
-        setActive4(current => !current);
-    };
-    const handleClick5 = () => {
-        setActive5(current => !current);
-    };
-
     const inactiveColor = "#bebebe"
-    const activeColor = "#5adbb5"
-
-    function hover1(e: any) {
-        if (active1 === false) {
-            e.target.style.background = "gray"
-        }
-    }
-    function unhover1(e: any) {
-        if (active1 === false) {
-            e.target.style.background = "#bebebe"
-        }
-    }
-    function hover2(e: any) {
-        if (active2 === false) {
-            e.target.style.background = "gray"
-        }
-    }
-    function unhover2(e: any) {
-        if (active2 === false) {
-            e.target.style.background = "#bebebe"
-        }
-    }
-    function hover3(e: any) {
-        if (active3 === false) {
-            e.target.style.background = "gray"
-        }
-    }
-    function unhover3(e: any) {
-        if (active3 === false) {
-            e.target.style.background = "#bebebe"
-        }
-    }
-
-    function hover5(e: any) {
-        if (active5 === false) {
-            e.target.style.background = "gray"
-        }
-    }
-    function unhover5(e: any) {
-        if (active5 === false) {
-            e.target.style.background = "#bebebe"
-        }
-    }
-
     return (
         <>
             <div style={{ display: "flex", justifyContent: "space-evenly" }}>
 
                 <Box >
-                    <Button onClick={handleClick1}
+                    <Button
                         style={{
                             display: "flex",
                             justifyContent: "center",
@@ -93,7 +27,6 @@ export default function RobotStatus() {
                     >
                         <div
                             style={{
-                                color: active1 ? "white" : "black",
                                 display: "flex",
                                 alignSelf: "center",
                                 fontSize: "22px"
@@ -102,8 +35,8 @@ export default function RobotStatus() {
                         </div>
                     </Button >
                 </Box>
-                <Box onMouseEnter={hover2} onMouseLeave={unhover2} >
-                    <Button onClick={handleClick2}
+                <Box>
+                    <Button
                         style={{
                             display: "flex",
                             justifyContent: "center",
@@ -112,12 +45,11 @@ export default function RobotStatus() {
                             height: "10rem",
                             borderRadius: "0.5rem",
                             boxShadow: "3px 3px 8px gray",
-                            backgroundColor: active2 ? activeColor : inactiveColor
+                            backgroundColor: inactiveColor
                         }}
                     >
                         <div
                             style={{
-                                color: active2 ? "white" : "black",
                                 display: "flex",
                                 alignSelf: "center",
                                 fontSize: "22px"
@@ -126,8 +58,8 @@ export default function RobotStatus() {
                         </div>
                     </Button>
                 </Box>
-                <Box onMouseEnter={hover3} onMouseLeave={unhover3} >
-                    <Button onClick={handleClick3}
+                <Box >
+                    <Button
                         style={{
                             display: "flex",
                             justifyContent: "center",
@@ -136,12 +68,11 @@ export default function RobotStatus() {
                             height: "10rem",
                             borderRadius: "0.5rem",
                             boxShadow: "3px 3px 8px gray",
-                            backgroundColor: active3 ? activeColor : inactiveColor
+                            backgroundColor: inactiveColor
                         }}
                     >
                         <div
                             style={{
-                                color: active3 ? "white" : "black",
                                 display: "flex",
                                 alignSelf: "center",
                                 fontSize: "22px"
@@ -151,8 +82,8 @@ export default function RobotStatus() {
                     </Button>
                 </Box>
 
-                <Box onMouseEnter={hover5} onMouseLeave={unhover5} >
-                    <Button onClick={handleClick5}
+                <Box >
+                    <Button
                         style={{
                             display: "flex",
                             justifyContent: "center",
@@ -162,13 +93,12 @@ export default function RobotStatus() {
                             height: "10rem",
                             borderRadius: "0.5rem",
                             boxShadow: "3px 3px 8px gray",
-                            backgroundColor: active5 ? activeColor : inactiveColor
+                            backgroundColor: inactiveColor
 
                         }}
                     >
                         <div
                             style={{
-                                color: active5 ? "white" : "black",
                                 display: "flex",
                                 alignSelf: "center",
                                 fontSize: "22px"
